9d35f35eb391730618a77dea1f008176dcb0288a
Sofia Guardia Guillan <sofia.guardia@stemyenergy.com>
2024-01-19 02:08:57 +0100

    Add class dark to container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,9 +5,9 @@ interface ContainerProps {
 export default function Container({ children }: ContainerProps) {
   return (
     <main
-      className="flex h-screen w-screen items-start justify-center bg-gradient-to-br 
-    from-white via-white to-stone-100
-    dark:from-neutral-900 dark:via-zinc-800 dark:to-slate-950
+      className="dark:to-slate- flex h-screen w-screen items-start justify-center 
+    overflow-hidden bg-gradient-to-br from-white
+    via-white to-stone-100 dark:from-neutral-900 dark:via-zinc-800
     "
     >
       <section className="w-full">{children}</section>
