d71de1c463b8b70d64c748c8b09af40fc40d513d
Sofia Guardia Guillan <sofia.guardia@stemyenergy.com>
2024-01-20 05:02:18 +0100

    Add keys of dictionary

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,27 @@
 import { Eye, EyeOff, HelpCircle } from "lucide-react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import { Button } from "./ui/button";
-import { useEffect, useState } from "react";
-import { ModalWithTitle } from "./ui/modals/Modal";
-
+import { SetStateAction, useEffect, useState } from "react";
+import { ModalWithTitle } from "./ui/Modal";
+import { Trans, useTranslation } from "next-i18next";
+import LanguageSwitcher from "./LanguageSwitcher";
+import Image from "next/image";
+import greenKey from "../../public/assets/greenKey.png";
+import yellowKey from "../../public/assets/yellowKey.png";
+import grayKey from "../../public/assets/grayKey.png";
+import { useRouter } from "next/router";
+import { Dictionary, getTranslation } from "@/data/dictionary";
+// import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 interface Props {
   word: string[];
-}
-function getRandomNumber(length: number) {
-  return Math.floor(Math.random() * length);
+  language: string;
+  setLanguage: React.Dispatch<SetStateAction<string>>;
+  locale: any;
 }
 
-export default function Header({ word }: Props) {
-  const randomIndex = getRandomNumber(word.length);
+export default function Header({ word, language, setLanguage, locale }: Props) {
   const [isActive, setIsActive] = useState(false);
-
+  const { t } = useTranslation();
   const [tip, setTip] = useState("");
   const [isModalVisible, setIsModalVisible] = useState(false);
   const openOnDeleteModal = () => setIsModalVisible(true);
@@ -24,23 +31,9 @@ export default function Header({ word }: Props) {
 
   return (
     <>
-      <header className="relative mt-4 flex items-center justify-center gap-10 py-0 font-header text-lg font-extrabold md:text-3xl lg:text-3xl">
+      <header className=" mt-4 flex w-full items-center justify-center gap-3 py-0">
         <div className="relative">
-          <span
-            className={`absolute -left-36 top-1/2 -translate-y-1/2 text-center font-sans text-3xl font-extrabold uppercase tracking-wider dark:text-slate-400
-        ${isActive ? "block" : "hidden"}`}
-          >
-            {isActive &&
-              tip &&
-              tip
-                .split("")
-                .map((character, index) => (
-                  <span key={index}>
-                    {index === randomIndex ? character : " _ "}
-                  </span>
-                ))}
-          </span>
-          <span className="absolute -right-[300px] top-1/2 -translate-y-1/2 text-center font-sans text-3xl font-extrabold uppercase tracking-wider dark:text-slate-400">
+          <span className="absolute -left-36 top-1/2 -translate-y-1/2 text-center font-sans text-3xl font-extrabold uppercase tracking-wider dark:text-slate-400">
             {word && word}
           </span>
 
@@ -52,7 +45,12 @@ export default function Header({ word }: Props) {
             {isActive ? <EyeOff /> : <Eye />}
           </Button>
         </div>
-        <h1 className="tracking-wider text-purpleBackGround dark:text-slate-400">
+        <LanguageSwitcher
+          locale={locale}
+          language={language}
+          setLanguage={setLanguage}
+        />
+        <h1 className="font-header tracking-wider text-purpleBackGround dark:text-slate-400">
           Wordle
         </h1>
         <ThemeSwitcher />
@@ -65,23 +63,94 @@ export default function Header({ word }: Props) {
         </Button>
       </header>
       <ModalWithTitle
-        title="TEST MODAL"
+        title={getTranslation("HowToPlayTitle", language)}
         isModalVisible={isModalVisible}
         setIsModalVisible={setIsModalVisible}
-        footer={<>TEST BUTTON</>}
+        maxWidth="2xl"
       >
-        Wordle es un rompecabezas de palabras divertido y agradable que
-        cualquiera puede jugar. Instrucciones:\n- Objetivo: El objetivo de
-        Wordle es adivinar una palabra oculta de cinco letras.\n- Adivinar:
-        Tienes seis intentos para adivinar la palabra correcta.\n- Haciendo
-        Adivinanzas: Envía una palabra de cinco letras y presiona enter.\n-
-        Retroalimentación: Después de cada intento, Wordle marca las letras
-        correctas en la posición correcta en verde. Las letras correctas en la
-        posición incorrecta se marcan en amarillo. Las letras incorrectas se
-        marcan en gris.\n- Ajustar Estrategia: Usa la retroalimentación para
-        ajustar tu estrategia y acercarte a la palabra correcta.\n- Ganar: Si
-        adivinas la palabra dentro de seis intentos, ¡ganas! De lo contrario,
-        Wordle revela la palabra correcta.
+        <ul className="list-inside list-disc text-pretty">
+          <h3>{getTranslation("HowToPlayInformation", language)}</h3>
+
+          <li>
+            <span className="font-bold tracking-wide text-indigo-500">
+              {getTranslation("HowToPlayObjectiveSpan", language)}
+            </span>{" "}
+            {getTranslation("HowToPlayObjective", language)}
+          </li>
+          <li>
+            <span className="font-bold tracking-wide text-indigo-500">
+              {getTranslation("HowToPlayGuessingSpan", language)}
+            </span>{" "}
+            {getTranslation("HowToPlayGuessing", language)}
+          </li>
+          <li>
+            <span className="font-bold tracking-wide text-indigo-500">
+              {getTranslation("HowToPlayMakingGuessesSpan", language)}
+            </span>{" "}
+            {getTranslation("HowToPlayMakingGuesses", language)}
+          </li>
+          <li>
+            <span className="font-bold tracking-wide text-indigo-500">
+              {getTranslation("HowToPlayFeedbackSpan", language)}
+            </span>{" "}
+            {getTranslation("HowToPlayFeedback", language)}
+            <ul className="list-inside">
+              <section className="flex ">
+                <Image width={25} height={20} src={greenKey} alt="green-key" />
+
+                <span className="mr-1">
+                  {getTranslation("HowToPlayCorrectAnswers", language)}
+                </span>
+
+                <span className="mr-1 font-bold text-greenCenterButton">
+                  {getTranslation("HowToPlayCorrectAnswersColor", language)}
+                </span>
+              </section>
+              <section className="flex ">
+                <Image width={25} height={30} src={yellowKey} alt="yelow-key" />
+
+                <span className="mr-1">
+                  {getTranslation("HowToPlayMissplacedAnswers", language)}
+                </span>
+
+                <span className="ml-1 font-bold text-yellowMarginButton ">
+                  {getTranslation("HowToPlayMissplacedAnswersColor", language)}
+                </span>
+              </section>
+              <section className="flex ">
+                <Image
+                  width={25}
+                  className=""
+                  height={30}
+                  src={grayKey}
+                  alt="gray-key"
+                />
+                <span className="ml-1">
+                  {getTranslation("HowToPlayIncorrectAnswers", language)}
+                </span>
+
+                <span className="ml-1 font-bold text-gray-700">
+                  {getTranslation("HowToPlayIncorrectAnswersColor", language)}{" "}
+                </span>
+              </section>
+            </ul>
+          </li>
+          <li>
+            <span className="font-bold tracking-wide text-indigo-500">
+              {getTranslation("HowToPlayAdjustingStrategySpan", language)}
+            </span>{" "}
+            <span>
+              {" "}
+              {getTranslation("HowToPlayAdjustingStrategy", language)}{" "}
+            </span>
+          </li>
+          <li>
+            <span className="font-bold tracking-wide text-indigo-500">
+              {getTranslation("HowToPlayWinningSpan", language)}
+            </span>{" "}
+            {getTranslation("HowToPlayWinning", language)}
+          </li>
+        </ul>
       </ModalWithTitle>
     </>
   );
